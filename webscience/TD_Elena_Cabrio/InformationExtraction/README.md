a0c36964fefd0d354da948f76bff3e78c8ff192a
Couvreur Alexis <alexiscouvreur.pro@gmail.com>
2018-03-08 02:17:44 +0100

    exercise 2 done

--- webscience/TD_Elena_Cabrio/InformationExtraction/README.md
+++ webscience/TD_Elena_Cabrio/InformationExtraction/README.md
@@ -2,10 +2,23 @@
 
 ## Exercise 1
 
-To annonate the text with the entity tag, it is a naive way.
+We first decompose the entity name, we add to our regular expression the full entity name, first name, last name, and pronouns.
+
+__Example : Catherine_Deneuve.txt__
+
+1. We extract the entity of the file :  
+From __"Catherine Deneuve"__ we create a regular expression matching : *"Catherine Deneuve" | "Catherine" | "Deneuve" | "she" | "She"*.  
+
+    
+2. We iterate over the file :  
+Each time our regular expression match we add the entity tag.
 
 ## Exercise 2
 
 ## Exercise 3
 
 ## Exercise 4
+
+## Go further
+
+Stanford nlp
--- webscience/TD_Elena_Cabrio/InformationExtraction/src/main/java/fr/alexisc/webscience/App.java
+++ webscience/TD_Elena_Cabrio/InformationExtraction/src/main/java/fr/alexisc/webscience/App.java
@@ -3,12 +3,14 @@ package fr.alexisc.webscience;
 import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
 /**
  * Main Application
- *
+ * <p>
  * It's purpose is to
  */
 public class App {
@@ -17,10 +19,10 @@ public class App {
     public static final String CORPUSES_ANNOTATED_PATH = "src/main/resources/corpuses/annotated/";
     public static final String ENTITY_LIST_PATH = "src/main/resources/entity_list.txt";
 
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
 
         File annotated = new File(CORPUSES_ANNOTATED_PATH);
-        if(!annotated.exists()) {
+        if (!annotated.exists()) {
             annotated.mkdirs();
         }
 
@@ -28,13 +30,31 @@ public class App {
 
         getCorpuses().forEach(corpus -> {
             String entityName = corpus.getName().split("\\.")[0];
+            Entity entity = new Entity(uriMap.get(entityName), corpus);
 
-                System.out.println("Processing `" + entityName + "` ...");
+            System.out.println("Processing `" + entityName + "` ...");
 
-                Annotator annotator = new Annotator(new Entity(uriMap.get(entityName), corpus));
+            Annotator annotator = new Annotator(entity);
 
             try {
+                System.out.println("Annotating the text ...");
                 annotator.annotate();
+                System.out.println(entityName + " annotated.");
+                String hasDateTriple = annotator.getBirthDateTriple(entity);
+                List<String> typeTriple = annotator.getTypeTriples(entity);
+
+                if(hasDateTriple != null){
+                    System.out.println("Triple <hasDate> found : " + hasDateTriple);
+                } else {
+                    System.out.println("No triple <hasDate> found.");
+                }
+
+                if(!typeTriple.isEmpty()){
+                    typeTriple.forEach(triple -> System.out.println("Triple <type> found : " + typeTriple));
+                } else {
+                    System.out.println("No triple <type> found.");
+                }
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -49,20 +69,20 @@ public class App {
         File folder = new File(CORPUSES_PATH);
         File[] files = folder.listFiles();
 
-        if(files == null){
+        if (files == null) {
             return Collections.emptyList();
         }
         return Arrays.stream(files).filter(File::isFile).collect(Collectors.toList());
     }
 
-    private static Map<String, URI> getURIMap(){
+    private static Map<String, URI> getURIMap() {
 
         Map<String, URI> uriMap = new HashMap<>();
         try {
             BufferedReader br = new BufferedReader(new FileReader(ENTITY_LIST_PATH));
 
             String line;
-            while((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 uriMap.put(line.split("/")[4], new URI(line));
             }
 
