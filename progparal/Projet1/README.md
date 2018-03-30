bd0b22cf26aa76df3a3bf2acb646c25461ad0526
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2018-03-30 15:44:25 +0200

    naive paralle

--- progparal/Projet1/README.md
+++ progparal/Projet1/README.md
@@ -31,3 +31,33 @@ use valgrind
 
 * Algo fonctionnel
 * Optimisation temps d'exec
+
+## Valgrind
+
+* Without free tmp tabs : 
+    ```
+    HEAP SUMMARY:
+    ==2720==     in use at exit: 4,960 bytes in 38 blocks
+    ==2720==   total heap usage: 77 allocs, 39 frees, 115,760 bytes allocated
+    ==2720== 
+    ==2720== LEAK SUMMARY:
+    ==2720==    definitely lost: 304 bytes in 16 blocks
+    ==2720==    indirectly lost: 1,472 bytes in 15 blocks
+    ==2720==      possibly lost: 864 bytes in 3 blocks
+    ==2720==    still reachable: 2,320 bytes in 4 blocks
+    ==2720==         suppressed: 0 bytes in 0 blocks
+    ```
+
+* With free : 
+    ```
+    HEAP SUMMARY:
+    ==2853==     in use at exit: 4,832 bytes in 30 blocks
+    ==2853==   total heap usage: 77 allocs, 47 frees, 115,760 bytes allocated
+    ==2853== 
+    ==2853== LEAK SUMMARY:
+    ==2853==    definitely lost: 1,200 bytes in 16 blocks
+    ==2853==    indirectly lost: 448 bytes in 7 blocks
+    ==2853==      possibly lost: 864 bytes in 3 blocks
+    ==2853==    still reachable: 2,320 bytes in 4 blocks
+    ==2853==         suppressed: 0 bytes in 0 blocks
+    ```
--- progparal/Projet1/couvreur.c
+++ progparal/Projet1/couvreur.c
@@ -7,37 +7,38 @@
 #define SUFFIX 1
 #define PREFIX 2
 
-#define max(a,b) \
-   ({ __typeof__ (a) _a = (a); \
+#define max(a, b) \
+	({ __typeof__ (a) _a = (a); \
        __typeof__ (b) _b = (b); \
      _a > _b ? _a : _b; })
 
 /* Structures */
-struct tablo {
-  int * tab;
-  int size;
+struct tablo
+{
+	int *tab;
+	int size;
 };
 
 /* Headers */
 
-void sum(struct tablo*, struct tablo*, int mode);
-void montee_sum(struct tablo*, struct tablo*);
-void descente_sum(struct tablo*, struct tablo*);
-void final_sum(struct tablo*, struct tablo*);
+void sum(struct tablo *, struct tablo *, int mode);
+void montee_sum(struct tablo *, struct tablo *);
+void descente_sum(struct tablo *, struct tablo *);
+void final_sum(struct tablo *, struct tablo *);
 
-void maximum(struct tablo*, struct tablo*, int mode);
-void montee_max(struct tablo*, struct tablo*);
-void descente_max(struct tablo*, struct tablo*);
-void final_max(struct tablo*, struct tablo*);
+void maximum(struct tablo *, struct tablo *, int mode);
+void montee_max(struct tablo *, struct tablo *);
+void descente_max(struct tablo *, struct tablo *);
+void final_max(struct tablo *, struct tablo *);
 
-void generateArray(struct tablo*);
-void printArray(struct tablo*);
-struct tablo * allocateTablo(int);
-void inverser_tablo(struct tablo*);
-
+void generateArray(struct tablo *);
+void printArray(struct tablo *);
+struct tablo *allocateTablo(int);
+void inverser_tablo(struct tablo *);
 
 /*  */
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 
 	/* args check */
 	/*if(argc != 2){
@@ -72,16 +73,16 @@ int main(int argc, char* argv[]){
 	printf("Tableau Q:\n");
 	printArray(&Q);
 
-	struct tablo* PSUM = allocateTablo(Q.size);
+	struct tablo *PSUM = allocateTablo(Q.size);
 	sum(&Q, PSUM, PREFIX);
 
 	printf("Tableau PSUM:\n");
 	printArray(PSUM);
-	
+
 	/*
 	 * 2. Calculer le sum-suffix de Q et les mettre dans un tableau SSUM
 	 */
-	struct tablo* SSUM = allocateTablo(Q.size);
+	struct tablo *SSUM = allocateTablo(Q.size);
 	sum(&Q, SSUM, SUFFIX);
 
 	printf("Tableau SSUM:\n");
@@ -90,7 +91,7 @@ int main(int argc, char* argv[]){
 	/*
 	 * 3. Calculer le max-suffix de PSUM et le mettre dans SMAX
 	 */
-	struct tablo* SMAX = allocateTablo(Q.size);
+	struct tablo *SMAX = allocateTablo(Q.size);
 	maximum(PSUM, SMAX, SUFFIX);
 
 	printf("Tableau SMAX:\n");
@@ -98,7 +99,7 @@ int main(int argc, char* argv[]){
 	/*
 	 * 4. Calculer le max-prefix de SSUM et le mettre dans PMAX
 	 */
-	struct tablo* PMAX;
+	struct tablo *PMAX;
 	PMAX = allocateTablo(Q.size);
 	maximum(SSUM, PMAX, PREFIX);
 
@@ -112,11 +113,13 @@ int main(int argc, char* argv[]){
 	 * 	3. M[i] = Ms[i] + Mp[i] - Q[i]
 	 */
 
-	struct tablo* Ms = allocateTablo(Q.size);
-	struct tablo* Mp = allocateTablo(Q.size);
-	struct tablo* M = allocateTablo(Q.size);
+	struct tablo *Ms = allocateTablo(Q.size);
+	struct tablo *Mp = allocateTablo(Q.size);
+	struct tablo *M = allocateTablo(Q.size);
 
-	for(int i = 0; i < Q.size; i++){
+#pragma omp parallel for
+	for (int i = 0; i < Q.size; i++)
+	{
 		Ms->tab[i] = PMAX->tab[i] - SSUM->tab[i] + Q.tab[i];
 		Mp->tab[i] = SMAX->tab[i] - PSUM->tab[i] + Q.tab[i];
 		M->tab[i] = Ms->tab[i] + Mp->tab[i] - Q.tab[i];
@@ -128,204 +131,274 @@ int main(int argc, char* argv[]){
 	int max_val = M->tab[0];
 	int start = 0;
 	int end = 0;
-	
-	#pragma omp parallel for 
-	for(int i = 1; i < M->size; i++){
-		if(M->tab[i] > max_val){
+
+#pragma omp parallel for reduction(max \
+								   : max_val)
+	for (int i = 1; i < M->size; i++)
+	{
+		if (M->tab[i] > max_val)
+		{
 			max_val = M->tab[i];
+		}
+	}
+
+	for (int i = 0; i < M->size; i++)
+	{
+		if (M->tab[i] == max_val)
+		{
 			start = i;
-			end = i;
-		} else if(M->tab[i] == max_val){
-			end = i;
+			break;
+		}
+	}
+
+	for (int i = start; i < M->size; i++)
+	{
+		if (M->tab[i] != max_val)
+		{
+			end = i - 1;
+			break;
 		}
 	}
 
 	printf("Max value is : %d within [%d, %d]\n", max_val, start + 1, end + 1);
 
-	
+	/*
+	 *
+	 * Affichage final
+	*/
+	printf("%d ", max_val);
+	for (int i = start; i < end; i++)
+	{
+		printf("%d ", Q.tab[i]);
+	}
+
+	printf("%d\n", Q.tab[end]);
 }
 
 /*---- SUM ---------------------------------------*/
 
-void sum(struct tablo * source, struct tablo * destination, int mode){
+void sum(struct tablo *source, struct tablo *destination, int mode)
+{
 
 	// TODO: inverser tableau
-	if(mode == SUFFIX){
+	if (mode == SUFFIX)
+	{
 		inverser_tablo(source);
 	}
 
-	struct tablo * a = allocateTablo(source->size*2);
+	struct tablo *a = allocateTablo(source->size * 2);
 	// TODO: do not reverse the array, iterate reverse way
-	montee_sum(source, a/*, mode*/);
+	montee_sum(source, a /*, mode*/);
 
-	struct tablo * b = allocateTablo(source->size*2);
-	descente_sum(a, b/*, mode*/);
-   
-	final_sum(a, b/*, mode*/);
+	struct tablo *b = allocateTablo(source->size * 2);
+	descente_sum(a, b /*, mode*/);
 
-	//destination = allocateTablo(source->size);
+	final_sum(a, b /*, mode*/);
 
 	// Tab b is source size * 2 so
-	for(int i = 0; i < source->size; i++){
+	for (int i = 0; i < source->size; i++)
+	{
 		destination->tab[i] = b->tab[source->size + i];
 	}
 
-	if(mode == SUFFIX){
+	if (mode == SUFFIX)
+	{
 		inverser_tablo(source);
 		inverser_tablo(destination);
 	}
+
+	free(a);
+	free(b);
 }
 
-void montee_sum(struct tablo * source, struct tablo * destination) {
-	for(int i = 0; i < source->size; i++){
+void montee_sum(struct tablo *source, struct tablo *destination)
+{
+	for (int i = 0; i < source->size; i++)
+	{
 		destination->tab[source->size + i] = source->tab[i];
 	}
-   
-	for(int l = log2(source->size) - 1 ; l >= 0; l--){
-		#pragma omp parallel for
-		for(int i = (int) pow(2, l) - 1; i <= ((int) pow(2, l+1)) - 1; i++){
-			destination->tab[i] = destination->tab[2 * i] + destination->tab[2 * i+1];
+
+	for (int l = log2(source->size) - 1; l >= 0; l--)
+	{
+#pragma omp parallel for
+		for (int i = (int)pow(2, l) - 1; i <= ((int)pow(2, l + 1)) - 1; i++)
+		{
+			destination->tab[i] = destination->tab[2 * i] + destination->tab[2 * i + 1];
 		}
 	}
 }
 
-void descente_sum(struct tablo * a, struct tablo * b) {
+void descente_sum(struct tablo *a, struct tablo *b)
+{
 
 	b->tab[1] = 0;
-	for(int k = 1; k <= log2(a->size) - 1; k++){
-		#pragma omp parallel for
-		for(int i = pow(2, k); i <=	(int)pow(2, k+1) - 1; i++){
-			if(i%2 == 0){
-				b->tab[i] = b->tab[i/2];
-			} else {
-				b->tab[i] = b->tab[i/2] + a->tab[i-1];
+	for (int k = 1; k <= log2(a->size) - 1; k++)
+	{
+#pragma omp parallel for
+		for (int i = pow(2, k); i <= (int)pow(2, k + 1) - 1; i++)
+		{
+			if (i % 2 == 0)
+			{
+				b->tab[i] = b->tab[i / 2];
+			}
+			else
+			{
+				b->tab[i] = b->tab[i / 2] + a->tab[i - 1];
 			}
 		}
 	}
 }
 
-void final_sum(struct tablo * a, struct tablo *b) {
+void final_sum(struct tablo *a, struct tablo *b)
+{
 
-	int m = log2(b->size/2);
-	#pragma omp parallel for
-  	for(int i = pow(2, m - 1); i <= (int)pow(2, m+1) - 1; i++){
+	int m = log2(b->size / 2);
+#pragma omp parallel for
+	for (int i = pow(2, m - 1); i <= (int)pow(2, m + 1) - 1; i++)
+	{
 		b->tab[i] = b->tab[i] + a->tab[i];
 	}
 }
 
 /*---- MAX ---------------------------------------*/
 
-void maximum(struct tablo * source, struct tablo * destination, int mode){
+void maximum(struct tablo *source, struct tablo *destination, int mode)
+{
 
 	// TODO: inverser tableau
-	if(mode == SUFFIX){
+	if (mode == SUFFIX)
+	{
 		inverser_tablo(source);
 	}
 
-	struct tablo * a = allocateTablo(source->size*2);
+	struct tablo *a = allocateTablo(source->size * 2);
 	// TODO: do not reverse the array, iterate reverse way
-	montee_max(source, a/*, mode*/);
+	montee_max(source, a /*, mode*/);
 
-	struct tablo * b = allocateTablo(source->size*2);
-	descente_max(a, b/*, mode*/);
-   
-	final_max(a, b/*, mode*/);
+	struct tablo *b = allocateTablo(source->size * 2);
+	descente_max(a, b /*, mode*/);
+
+	final_max(a, b /*, mode*/);
 
 	//destination = allocateTablo(source->size);
 
 	// Tab b is source size * 2 so
-	for(int i = 0; i < source->size; i++){
+	for (int i = 0; i < source->size; i++)
+	{
 		destination->tab[i] = b->tab[source->size + i];
 	}
 
-	if(mode == SUFFIX){
+	if (mode == SUFFIX)
+	{
 		inverser_tablo(source);
 		inverser_tablo(destination);
 	}
+
+	free(a);
+	free(b);
 }
 
-void montee_max(struct tablo * source, struct tablo * destination) {
-	for(int i = 0; i < source->size; i++){
+void montee_max(struct tablo *source, struct tablo *destination)
+{
+	for (int i = 0; i < source->size; i++)
+	{
 		destination->tab[source->size + i] = source->tab[i];
 	}
-   
-	for(int l = log2(source->size) - 1 ; l >= 0; l--){
-		#pragma omp parallel for
-		for(int i = (int) pow(2, l) - 1; i <= ((int) pow(2, l+1)) - 1; i++){
-			destination->tab[i] = max(destination->tab[2 * i], destination->tab[2 * i+1]);
+
+	for (int l = log2(source->size) - 1; l >= 0; l--)
+	{
+#pragma omp parallel for
+		for (int i = (int)pow(2, l) - 1; i <= ((int)pow(2, l + 1)) - 1; i++)
+		{
+			destination->tab[i] = max(destination->tab[2 * i], destination->tab[2 * i + 1]);
 		}
 	}
 }
 
-void descente_max(struct tablo* a, struct tablo* b){
+void descente_max(struct tablo *a, struct tablo *b)
+{
 	b->tab[1] = INT_MIN;
-	for(int k = 1; k <= log2(a->size) - 1; k++){
-		#pragma omp parallel for
-		for(int i = pow(2, k); i <=	(int)pow(2, k+1) - 1; i++){
-			if(i%2 == 0){
-				b->tab[i] = b->tab[i/2];
-			} else {
-				b->tab[i] = max(b->tab[i/2], a->tab[i-1]);
+	for (int k = 1; k <= log2(a->size) - 1; k++)
+	{
+#pragma omp parallel for
+		for (int i = pow(2, k); i <= (int)pow(2, k + 1) - 1; i++)
+		{
+			if (i % 2 == 0)
+			{
+				b->tab[i] = b->tab[i / 2];
+			}
+			else
+			{
+				b->tab[i] = max(b->tab[i / 2], a->tab[i - 1]);
 			}
 		}
 	}
 }
-void final_max(struct tablo* a, struct tablo* b){
-	int m = log2(b->size/2);
-	#pragma omp parallel for
-  	for(int i = pow(2, m - 1); i <= (int)pow(2, m+1) - 1; i++){
+void final_max(struct tablo *a, struct tablo *b)
+{
+	int m = log2(b->size / 2);
+
+#pragma omp parallel for
+	for (int i = pow(2, m - 1); i <= (int)pow(2, m + 1) - 1; i++)
+	{
 		b->tab[i] = max(b->tab[i], a->tab[i]);
 	}
 }
 
 /*---- Utils ---------------------------------------*/
-void inverser_tablo(struct tablo * a){
+void inverser_tablo(struct tablo *a)
+{
 	int i = a->size - 1;
 	int j = 0;
-  	while(i > j) {
+	while (i > j)
+	{
 		int temp = a->tab[i];
-	    a->tab[i] = a->tab[j];
-	    a->tab[j] = temp;
-    	i--;
-	    j++;
-  	}
+		a->tab[i] = a->tab[j];
+		a->tab[j] = temp;
+		i--;
+		j++;
+	}
 }
 
-void generateArray(struct tablo * s) {
-  //construction d'un tableau pour tester
-  s->size=16;
-  s->tab=malloc(s->size*sizeof(int));
-  s->tab[0]=3;
-  s->tab[1]=2;
-  s->tab[2]=-7;
-  s->tab[3]=11;
-  s->tab[4]=10;
-  s->tab[5]=-6;
-  s->tab[6]=4;
-  s->tab[7]=9;
-  s->tab[8]=-6;
-  s->tab[9]=1;
-  s->tab[10]=-2;
-  s->tab[11]=-3;
-  s->tab[12]=4;
-  s->tab[13]=-3;
-  s->tab[14]=0;
-  s->tab[15]=2;
+void generateArray(struct tablo *s)
+{
+	//construction d'un tableau pour tester
+	s->size = 16;
+	s->tab = malloc(s->size * sizeof(int));
+	s->tab[0] = 3;
+	s->tab[1] = 2;
+	s->tab[2] = -7;
+	s->tab[3] = 11;
+	s->tab[4] = 10;
+	s->tab[5] = -6;
+	s->tab[6] = 4;
+	s->tab[7] = 9;
+	s->tab[8] = -6;
+	s->tab[9] = 1;
+	s->tab[10] = -2;
+	s->tab[11] = -3;
+	s->tab[12] = 4;
+	s->tab[13] = -3;
+	s->tab[14] = 0;
+	s->tab[15] = 2;
 }
 
-void printArray(struct tablo * tmp) {
-  printf("---- Array of size %i ---- \n", tmp->size);
-  int size = tmp->size;
-  int i;
-  for (i = 0; i < size; ++i) {
-    printf("%i ", tmp->tab[i]);
-  }
-  printf("\n");
+void printArray(struct tablo *tmp)
+{
+	printf("---- Array of size %i ---- \n", tmp->size);
+	int size = tmp->size;
+	int i;
+	for (i = 0; i < size; ++i)
+	{
+		printf("%i ", tmp->tab[i]);
+	}
+	printf("\n");
 }
 
-struct tablo * allocateTablo(int size) {
-  struct tablo * tmp = malloc(sizeof(struct tablo));
-  tmp->size = size;
-  tmp->tab = malloc(size*sizeof(int));
-  return tmp;
+struct tablo *allocateTablo(int size)
+{
+	struct tablo *tmp = malloc(sizeof(struct tablo));
+	tmp->size = size;
+	tmp->tab = malloc(size * sizeof(int));
+	return tmp;
 }
