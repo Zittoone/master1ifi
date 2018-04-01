5f80fd9d90f866a235f07b287d27cad3290b8fad
Couvreur Alexis <alexiscouvreur.pro@gmail.com>
2018-04-01 19:38:22 +0200

    generating files with known answer

--- progparal/Projet1/generate_2n.c
+++ progparal/Projet1/generate_2n.c
@@ -3,6 +3,10 @@
 #include <math.h>
 #include <time.h>
 
+#define max_number 45000
+#define minimum_number -45000
+
+
 int main(int argc, char *argv[])
 {
 
@@ -16,9 +20,9 @@ int main(int argc, char *argv[])
 
     int iterations = pow(2, atoi(argv[1]));
     for(int i = 0; i < iterations - 1; i++){
-        printf("%d ", (rand() % 100) - 50);
+        printf("-2 ");
     }
 
-    printf("%d\n", (rand() % 100) - 50);
+    printf("1\n");
 
 }
