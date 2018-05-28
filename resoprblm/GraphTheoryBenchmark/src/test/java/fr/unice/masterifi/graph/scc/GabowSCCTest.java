4baa0af4773d1f153ef3a0248f7bd0ba28d74aae
Couvreur Alexis <alexiscouvreur.pro@gmail.com>
2018-05-28 21:55:11 +0200

    more tests

--- resoprblm/GraphTheoryBenchmark/src/test/java/fr/unice/masterifi/graph/scc/GabowSCCTest.java
+++ resoprblm/GraphTheoryBenchmark/src/test/java/fr/unice/masterifi/graph/scc/GabowSCCTest.java
@@ -11,12 +11,11 @@ import static org.junit.Assert.*;
  */
 public class GabowSCCTest {
 
-    DirectedGraph g;
     GabowSCC scc;
 
     @Before
     public void setUp() throws Exception {
-        g = new DirectedGraph(8);
+        DirectedGraph g = new DirectedGraph(8);
         g.addEdge(0, 1); // a -> b
 
         g.addEdge(1, 2); // b -> c
@@ -45,56 +44,121 @@ public class GabowSCCTest {
     @Test
     public void isStronglyConnectedA() throws Exception {
 
-        /* A */
-        assertEquals(true, scc.isStronglyConnected(0, 1));
-        assertEquals(false, scc.isStronglyConnected(0, 2));
-        assertEquals(false, scc.isStronglyConnected(0, 3));
-        assertEquals(true, scc.isStronglyConnected(0, 4));
-        assertEquals(false, scc.isStronglyConnected(0, 5));
-        assertEquals(false, scc.isStronglyConnected(0, 6));
-        assertEquals(false, scc.isStronglyConnected(0, 7));
+        int s = 0;
+        assertEquals(true, scc.isStronglyConnected(s, 0)); // a sc to a
+        assertEquals(true, scc.isStronglyConnected(s, 1)); // a sc to b
+        assertEquals(true, scc.isStronglyConnected(s, 4)); // a sc to e
 
-        /* B */
-        assertEquals(false, scc.isStronglyConnected(1, 2));
-        assertEquals(false, scc.isStronglyConnected(1, 3));
-        assertEquals(true, scc.isStronglyConnected(1, 4));
-        assertEquals(false, scc.isStronglyConnected(1, 5));
-        assertEquals(false, scc.isStronglyConnected(1, 6));
-        assertEquals(false, scc.isStronglyConnected(1, 7));
+        assertEquals(false, scc.isStronglyConnected(s, 2)); // a sc to c
+        assertEquals(false, scc.isStronglyConnected(s, 3)); // a sc to d
+        assertEquals(false, scc.isStronglyConnected(s, 5)); // a sc to f
+        assertEquals(false, scc.isStronglyConnected(s, 6)); // a sc to g
+        assertEquals(false, scc.isStronglyConnected(s, 7)); // a sc to h
 
-        /* C */
-        assertEquals(true, scc.isStronglyConnected(2, 3));
-        assertEquals(false, scc.isStronglyConnected(2, 4));
-        assertEquals(false, scc.isStronglyConnected(2, 5));
-        assertEquals(false, scc.isStronglyConnected(2, 6));
-        assertEquals(true, scc.isStronglyConnected(2, 7));
+    }
 
-        /* D */
-        assertEquals(false, scc.isStronglyConnected(3, 4));
-        assertEquals(false, scc.isStronglyConnected(3, 5));
-        assertEquals(false, scc.isStronglyConnected(3, 6));
-        assertEquals(true, scc.isStronglyConnected(3, 7));
+    @Test
+    public void isStronglyConnectedB() throws Exception {
 
-        /* E */
-        assertEquals(false, scc.isStronglyConnected(4, 5));
-        assertEquals(false, scc.isStronglyConnected(4, 6));
-        assertEquals(false, scc.isStronglyConnected(4, 7));
+        int s = 1;
+        assertEquals(true, scc.isStronglyConnected(s, 0)); // b sc to a
+        assertEquals(true, scc.isStronglyConnected(s, 1)); // b sc to b
+        assertEquals(true, scc.isStronglyConnected(s, 4)); // b sc to e
 
-        /* F */
-        assertEquals(true, scc.isStronglyConnected(5, 6));
-        assertEquals(false, scc.isStronglyConnected(5, 7));
+        assertEquals(false, scc.isStronglyConnected(s, 2)); // b sc to c
+        assertEquals(false, scc.isStronglyConnected(s, 3)); // b sc to d
+        assertEquals(false, scc.isStronglyConnected(s, 5)); // b sc to f
+        assertEquals(false, scc.isStronglyConnected(s, 6)); // b sc to g
+        assertEquals(false, scc.isStronglyConnected(s, 7)); // b sc to h
 
-        /* G */
-        assertEquals(false, scc.isStronglyConnected(6, 7));
+    }
+
+    @Test
+    public void isStronglyConnectedC() throws Exception {
+        int s = 2;
+        assertEquals(true, scc.isStronglyConnected(s, 2)); // c sc to c
+        assertEquals(true, scc.isStronglyConnected(s, 3)); // c sc to d
+        assertEquals(true, scc.isStronglyConnected(s, 7)); // c sc to h
+
+        assertEquals(false, scc.isStronglyConnected(s, 0)); // c sc to a
+        assertEquals(false, scc.isStronglyConnected(s, 1)); // c sc to b
+        assertEquals(false, scc.isStronglyConnected(s, 4)); // c sc to e
+        assertEquals(false, scc.isStronglyConnected(s, 5)); // c sc to f
+        assertEquals(false, scc.isStronglyConnected(s, 6)); // c sc to g
+    }
+
+    @Test
+    public void isStronglyConnectedD() throws Exception {
+        int s = 3;
+        assertEquals(true, scc.isStronglyConnected(s, 2)); // d sc to c
+        assertEquals(true, scc.isStronglyConnected(s, 3)); // d sc to d
+        assertEquals(true, scc.isStronglyConnected(s, 7)); // d sc to h
+
+        assertEquals(false, scc.isStronglyConnected(s, 0)); // d sc to a
+        assertEquals(false, scc.isStronglyConnected(s, 1)); // d sc to b
+        assertEquals(false, scc.isStronglyConnected(s, 4)); // d sc to e
+        assertEquals(false, scc.isStronglyConnected(s, 5)); // d sc to f
+        assertEquals(false, scc.isStronglyConnected(s, 6)); // d sc to g
+    }
+
+    @Test
+    public void isStronglyConnectedE() throws Exception {
+        int s = 4;
+        assertEquals(true, scc.isStronglyConnected(s, 0)); // e sc to a
+        assertEquals(true, scc.isStronglyConnected(s, 1)); // e sc to b
+        assertEquals(true, scc.isStronglyConnected(s, 4)); // e sc to e
+
+        assertEquals(false, scc.isStronglyConnected(s, 2)); // e sc to c
+        assertEquals(false, scc.isStronglyConnected(s, 3)); // e sc to d
+        assertEquals(false, scc.isStronglyConnected(s, 5)); // e sc to f
+        assertEquals(false, scc.isStronglyConnected(s, 6)); // e sc to g
+        assertEquals(false, scc.isStronglyConnected(s, 7)); // e sc to h
+    }
+
+    @Test
+    public void isStronglyConnectedF() throws Exception {
+        int s = 5;
+        assertEquals(true, scc.isStronglyConnected(s, 5)); // f sc to f
+        assertEquals(true, scc.isStronglyConnected(s, 6)); // f sc to g
+
+        assertEquals(false, scc.isStronglyConnected(s, 0)); // f sc to a
+        assertEquals(false, scc.isStronglyConnected(s, 1)); // f sc to b
+        assertEquals(false, scc.isStronglyConnected(s, 4)); // f sc to e
+        assertEquals(false, scc.isStronglyConnected(s, 2)); // f sc to c
+        assertEquals(false, scc.isStronglyConnected(s, 3)); // f sc to d
+        assertEquals(false, scc.isStronglyConnected(s, 7)); // f sc to h
+    }
+
+    @Test
+    public void isStronglyConnectedG() throws Exception {
+        int s = 6;
+        assertEquals(true, scc.isStronglyConnected(s, 5)); // g sc to f
+        assertEquals(true, scc.isStronglyConnected(s, 6)); // g sc to g
+
+        assertEquals(false, scc.isStronglyConnected(s, 0)); // g sc to a
+        assertEquals(false, scc.isStronglyConnected(s, 1)); // g sc to b
+        assertEquals(false, scc.isStronglyConnected(s, 4)); // g sc to e
+        assertEquals(false, scc.isStronglyConnected(s, 2)); // g sc to c
+        assertEquals(false, scc.isStronglyConnected(s, 3)); // g sc to d
+        assertEquals(false, scc.isStronglyConnected(s, 7)); // g sc to h
+    }
+
+    @Test
+    public void isStronglyConnectedH() throws Exception {
+        int s = 7;
+        assertEquals(true, scc.isStronglyConnected(s, 2)); // h sc to c
+        assertEquals(true, scc.isStronglyConnected(s, 3)); // h sc to d
+        assertEquals(true, scc.isStronglyConnected(s, 7)); // h sc to h
+
+        assertEquals(false, scc.isStronglyConnected(s, 0)); // h sc to a
+        assertEquals(false, scc.isStronglyConnected(s, 1)); // h sc to b
+        assertEquals(false, scc.isStronglyConnected(s, 4)); // h sc to e
+        assertEquals(false, scc.isStronglyConnected(s, 5)); // h sc to f
+        assertEquals(false, scc.isStronglyConnected(s, 6)); // h sc to g
     }
 
     @Test
     public void getCount() throws Exception {
         assertEquals(3, scc.getCount());
     }
-
-    @Test
-    public void id() throws Exception {
-    }
-
 }
