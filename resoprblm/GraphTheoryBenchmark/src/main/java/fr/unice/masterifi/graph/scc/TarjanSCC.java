715305007b315b3bf7e997a35e6b49d3d2bf9d6c
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2018-05-29 13:33:18 +0200

    tarjan impl with debug output

--- resoprblm/GraphTheoryBenchmark/src/main/java/fr/unice/masterifi/graph/scc/TarjanSCC.java
+++ resoprblm/GraphTheoryBenchmark/src/main/java/fr/unice/masterifi/graph/scc/TarjanSCC.java
@@ -1,7 +1,9 @@
 package fr.unice.masterifi.graph.scc;
 
 import fr.unice.masterifi.graph.DirectedGraph;
+import fr.unice.masterifi.graph.Utils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
@@ -11,34 +13,82 @@ import java.util.Stack;
 public class TarjanSCC implements SCC {
 
     private int num;
+    private int[] nums;
+    private int[] numAccessible;
+    private boolean[] dans_P;
     private Stack<Integer> P;
     private List<List<Integer>> partition;
 
     public TarjanSCC(DirectedGraph graph) {
         num = 0;
         P = new Stack<>();
+        nums = new int[graph.V()];
+        numAccessible = new int[graph.V()];
+        dans_P = new boolean[graph.V()];
+        partition = new ArrayList<>();
 
-        for(int v = 0; v < graph.V(); v++) {
-
+        for (int i = 0; i < graph.V(); i++) {
+            nums[i] = -1;
         }
+        for(int v = 0; v < graph.V(); v++) {
+            if(nums[v] == -1) {
+                parcours(graph, v);
+            }
+        }
+
+        for (List<Integer> l : partition)
+            System.out.println(Utils.toStringListCustom(l, Utils::toAlphabet));
     }
 
     private void parcours(DirectedGraph graph, int v) {
+        nums[v] = num;
+        numAccessible[v] = num;
+        num++;
+        P.push(v);
+        dans_P[v] = true;
 
+        System.out.println("Parcours de " + Utils.toAlphabet(v) + "; P=" + Utils.toStringListCustom(P, Utils::toAlphabet));
+
+        for(Integer w : graph.adj(v)) {
+            if(nums[w] == -1) {
+                parcours(graph, w);
+                numAccessible[v] = Math.min(numAccessible[v], numAccessible[w]);
+            } else if (dans_P[w]) {
+                numAccessible[v] = Math.min(numAccessible[v], nums[w]);
+            }
+        }
+
+        if(numAccessible[v] == nums[v]) {
+            int w;
+            List<Integer> C = new ArrayList<>();
+            do {
+                w = P.pop();
+                dans_P[w] = false;
+                C.add(w);
+            } while(w != v);
+
+            partition.add(C);
+        }
     }
 
     @Override
     public boolean isStronglyConnected(int v, int w) {
+        for (List<Integer> l : partition)
+            if(l.contains(v) && l.contains(w))
+                return true;
         return false;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return partition.size();
     }
 
     @Override
     public int id(int v) {
+        for (List<Integer> l : partition)
+            if(l.contains(v))
+                return partition.indexOf(l);
         return 0;
     }
 }
